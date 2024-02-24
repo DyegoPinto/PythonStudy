61414025d42aaf881d6bcf6122b90632ef6c7a99
Dyego Belmont <dyegosp10@yahoo.com.br>
2024-02-24 20:13:47 +0000

    adding new uber file

--- src/download_sample.py
+++ src/download_sample.py
@@ -40,3 +40,25 @@ for path in link:
 
 
 # %%
+        
+# Main Code - Download of 2 data sources
+
+link = 'https://github.com/dbaassists/YouTube/blob/main/ArquivoDados/trips_data_new.csv'
+
+# Craetion of the directory
+directory = 'C:/Users/dyego/PythonProject/PythonStudy/PythonStudy/Files/UBER'
+file_name = 'NoneExistent'
+
+os.makedirs(directory,exist_ok=True)
+# %%
+
+response = requests.get(link)
+
+file_name = 'trips_data_new.csv'
+
+directory_delivery = f'{directory}/{file_name}'  
+
+with open(directory_delivery, 'wb') as download:
+    download.write(response.content)
+
+# %%
