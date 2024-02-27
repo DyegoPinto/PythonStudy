3d15692c4b379ce498c390303cc4a34b39f1225f
Dyego Belmont <dyegosp10@yahoo.com.br>
2024-02-27 18:11:29 +0000

    adding new study

--- src/SQL_inserts_python.py
+++ src/SQL_inserts_python.py
@@ -2,6 +2,7 @@
 
 # import of libraries
 import pandas as pd
+import numpy as np
 from user_libraries.environment_setup import importa_arquivo_videos_youtube 
 
 # display setup 
@@ -13,13 +14,13 @@ pd.options.display.float_format = "{:.2f}".format
 
 # 99 - ENVIRONMENT SETUP
 
-source_file = 'C:/Users/dyego/PandasProject/Files/videos/USvideos.csv'
-destination_file = 'C:/Users/dyego/PandasProject/Files/videos/videos_youtube.csv'
-param_json = "C:/Users/dyego/PandasProject/src/param/config.json"
+source_file = 'C:/Users/dyego/PythonProject/PythonStudy/PythonStudy/Files/videos/USvideos.csv'
+destination_file = 'C:/Users/dyego/PythonProject/PythonStudy/PythonStudy/Files/videos/videos_youtube.csv'
+param_json = "C:/Users/dyego/PythonProject/PythonStudy/PythonStudy/src/param/config.json"
 
-importa_arquivo_videos_youtube(source_file
-              , destination_file
-              , param_json)
+#importa_arquivo_videos_youtube(source_file
+#              , destination_file
+#              , param_json)
 
 
 # %%
@@ -55,4 +56,21 @@ df_grouped = pd.concat([df_grouped, df_new_info],ignore_index=True)
 df_grouped.query('NomCanal == "new_row"')
 # %%
 
+# DELETE 01 - NOT OPTIMIZED
+
+
+#df_grouped = df_grouped.drop(labels=[13])
+
+#we need to consult first, and then pass the index we want to remove
+#df_grouped.drop(labels=[13], inplace=True)
+
+
 df_grouped
+# %%
+
+# DELETE 02 - OPTIMIZED1
+
+delete_condition = np.where(df_grouped['NomCanal'] == 'ibighit')[0]
+
+df_grouped.drop(delete_condition, inplace=True)
+# %%
